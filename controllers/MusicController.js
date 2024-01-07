72fd99575346eac0789113ff0d2c82b1faa0481f
Oleg <matyiokin2002@gmail.com>
2024-01-08 01:21:03 +0200

    Added basic REST functionality but witout proper validation...

--- controllers/MusicController.js
+++ controllers/MusicController.js
@@ -1,45 +1,66 @@
 import FileService from "../services/FileService.js";
 import MusicService from "../services/MusicService.js";
-class MusicController{
-    async create(req,res){
-        if(!req.files){
-            return res.status(500).json({message:"File not uploaded."})
-        }
-        try {
-            const files = await FileService.saveFiles(req.files);
-            if(!files){
-                return res.status(500).json({message:"File was not saved."})
-            }
-            const createdMusic = await MusicService.create({author:req.body.author, info:req.body.info,poster: files.image, audio:files.audio})
-            res.json(createdMusic);
-        }catch (e){
-            res.status(500).json(e);
-        }
+class MusicController {
+  async create(req, res) {
+    if (!req.files) {
+      return res.status(500).json({ message: "File not uploaded." });
     }
-    async getAll(req,res){
-        try {
-            const allMusic = await MusicService.getAll();
-            res.json(allMusic);
-        }catch (e){
-            res.status(500).json(e);
-        }
+    try {
+      const files = await FileService.saveFiles(req.files);
+      if (!files) {
+        return res.status(500).json({ message: "File was not saved." });
+      }
+      const createdMusic = await MusicService.create({
+        author: req.body.author,
+        info: req.body.info,
+        poster: files.image,
+        audio: files.audio,
+      });
+      res.json(createdMusic);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async getById(req,res){
-        const id = req.params.id;
-        try {
-            const music = await MusicService.getById(id);
-        }catch (e) {
-
-        }
-
+  }
+  async getAll(req, res) {
+    try {
+      const allMusic = await MusicService.getAll();
+      res.json(allMusic);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async update(req,res){
-
+  }
+  async getById(req, res) {
+    const id = req.params.id;
+    try {
+      const music = await MusicService.getById(id);
+      res.json(music);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async delete(req,res){
-
+  }
+  async update(req, res) {
+    try {
+      const { poster, audio } = req.files;
+      console.log(audio.name);
+      const updated = await MusicService.update(
+        req.body,
+        poster.name,
+        audio.name
+      );
+      res.json({ message: "Updated successfuly", previousMusic: updated });
+    } catch (e) {
+      res.status(500).json(e);
     }
-
+  }
+  async delete(req, res) {
+    const id = req.params.id;
+    try {
+      const deletedMusic = await MusicService.delete(id);
+      res.json(deletedMusic);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
 }
 
-export default new MusicController()
+export default new MusicController();
--- services/MusicService.js
+++ services/MusicService.js
@@ -1,22 +1,36 @@
 import Music from "../models/Music.js";
 import mongoose from "mongoose";
-class MusicService{
-    async create(music){
-        const {author, poster, info, audio} = music;
-        return await Music.create({author, info, poster:poster.fileName, audio:audio.fileName});
-    }
-    async getAll(){
-        return await Music.find({});
-    }
-    async getById(id){
-
-    }
-    async update(){
-
-    }
-    async delete(id){
-
-    }
+class MusicService {
+  async create(music) {
+    const { author, poster, info, audio } = music;
+    return await Music.create({
+      author,
+      info,
+      poster: poster.fileName,
+      audio: audio.fileName,
+    });
+  }
+  async getAll() {
+    return await Music.find({});
+  }
+  async getById(id) {
+    const music = await Music.findById(id);
+    return music;
+  }
+  async update(music, poster, audio) {
+    const updatedMusic = await Music.findByIdAndUpdate(music.id, {
+      author: music.author,
+      info: music.info,
+      poster: poster,
+      audio: audio,
+    });
+    console.log(updatedMusic);
+    return updatedMusic;
+  }
+  async delete(id) {
+    const deletedMusic = await Music.findByIdAndDelete(id);
+    return deletedMusic;
+  }
 }
 
 export default new MusicService();
